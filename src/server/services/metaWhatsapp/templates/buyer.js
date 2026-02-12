0064a199b4fcdb26557dbe68f361072a8e983525
Nemanja Savatic <savatic.nemanja@gmail.com>
2026-02-12 19:32:08 +0100

    test service

--- src/server/services/metaWhatsapp/templates/buyer.js
+++ src/server/services/metaWhatsapp/templates/buyer.js
@@ -241,11 +241,6 @@ export const createBuyerTemplates = ({
           parameters: [
             {
               type: "text",
-              parameter_name: "bid_id",
-              text: sanitizedBidId,
-            },
-            {
-              type: "text",
               parameter_name: "bid_details",
               text: sanitizedBidDetails,
             },
