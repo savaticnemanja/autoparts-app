4882530d4c4ed0d6a96d5fb4438784ffb80fc924
Nemanja Savatic <savatic.nemanja@gmail.com>
2026-02-12 19:17:51 +0100

    fixed parts_customer_offer

--- src/server/services/metaWhatsapp/templates/buyer.js
+++ src/server/services/metaWhatsapp/templates/buyer.js
@@ -111,11 +111,6 @@ export const createBuyerTemplates = ({
           parameters: [
             {
               type: "text",
-              parameter_name: "bid_id",
-              text: sanitizedBidId,
-            },
-            {
-              type: "text",
               parameter_name: "bid_details",
               text: sanitizedBidDetails,
             },
